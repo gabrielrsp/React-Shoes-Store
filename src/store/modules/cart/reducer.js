9f9088656d6a57a813366310472ca0cb2248f606
gabrielrsp <gabrielrsp18@gmail.com>
2020-02-05 22:09:40 -0300

    implement stock update

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -5,16 +5,9 @@ export default function cart(state = [], action) {
   switch (action.type) {
     case '@cart/ADD_SUCCESS':
       return produce(state, draft => {
-        const productIndex = draft.findIndex(p => p.id === action.product.id);
+        const { product } = action;
 
-        if (productIndex >= 0) {
-          draft[productIndex].amount += 1;
-        } else {
-          draft.push({
-            ...action.product,
-            amount: 1,
-          });
-        }
+        draft.push(product)
       });
     case '@cart/REMOVE':
       return produce(state, draft =>{
@@ -24,11 +17,7 @@ export default function cart(state = [], action) {
           draft.splice(productIndex, 1);
         }
       });
-    case '@cart/UPDATE_AMOUNT': {
-      if (action.amount <= 0) {
-        return state;
-      }
-
+    case '@cart/UPDATE_AMOUNT_SUCCESS': {
       return produce(state, draft => {
         const productIndex = draft.findIndex(p => p.id === action.id);
 
